060828324b5d8952418d39ccac7a38cedda071c8
John Skandalakis <skandaloptagon@gmail.com>
2018-02-22 00:17:07 -0500

    Comprehensive logging

--- DBManager.py
+++ DBManager.py
@@ -6,10 +6,17 @@ Author: John Skandalakis
 '''
 
 import sqlite3
+import logging
+
+# Always use a logger.
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class DBManager:
 
     def __init__(self, dbfile):
+        logger.info("Initializing the database manager")
+
         self.conn = sqlite3.connect(dbfile)
 
         self.c = self.conn.cursor()
@@ -18,7 +25,7 @@ class DBManager:
             self.c.execute('''CREATE TABLE appointments
                      (date text, time text, description text)''')
         except:
-            print("Omitting Query: Table already exists")
+            logger.warn("The table already exists. Omitting query.")
 
     def present_appointments(self, query=None):
         '''
@@ -26,6 +33,9 @@ class DBManager:
         :param query:
         :return:
         '''
+
+        logger.info("Querying db for appointments matching query: {}".format(query))
+
         if query is None: # A simple query if the user provides no text
             self.c.execute("select * from appointments")
         else: #Search every column
@@ -45,4 +55,5 @@ class DBManager:
         :param description:
         :return:
         '''
+        logger.info("Adding appointment to database. {}-{}: {}".format(date, time, description))
         return self.c.execute("insert into appointments values (?, ?, ?)", (date, time, description))
--- RestAPI.py
+++ RestAPI.py
@@ -2,7 +2,13 @@ from flask import Flask, request, render_template, redirect, send_from_directory
 from DBManager import *
 from json import dumps
 import argparse
+import logging
 
+# Always use a logger. Never print
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Make things easier for people who want to control attributes.
 parser = argparse.ArgumentParser(description='Runs a system which handles appointments.')
 parser.add_argument('-H', '--host', type=str, default='localhost',
                     help='The hostname to access this. To make public use 0.0.0.0. Default: \'localhost\'')
@@ -12,16 +18,20 @@ parser.add_argument('-d', '--dbfile', type=str,
                     default=':memory:', help='The name of the file the database uses. Default: \':memory:\'')
 args = parser.parse_args()
 
+# Create the flask applet.
 app = Flask(__name__, static_url_path='')
 
+# Our manager to access the database
 dbm = DBManager(args.dbfile)
 
 @app.route("/")
 def home():
+    logger.info("Rendering home page")
     return render_template("home.html")
 
 @app.route("/search")
 def search_appointments():
+    logger.info("Searching for appointments")
     if len(request.args) != 1:
         return dumps(dbm.present_appointments())
     query = request.args["query"]
@@ -29,14 +39,11 @@ def search_appointments():
 
 @app.route("/new")
 def create_appointment():
+    logger.info("Creating new appointment")
     date = request.args.get("date")
     time = request.args.get("time")
     description = request.args.get("description")
     dbm.add_appointment_to_db(date, time, description)
     return redirect('/')
-
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory('js', path)
 
 app.run(args.host, args.port)
